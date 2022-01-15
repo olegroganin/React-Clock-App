e1d70da1c599eeddf141ba7799d4fdd810b760f6
Oleg Roganin <94141484+olegroganin@users.noreply.github.com>
2022-01-15 17:05:23 +0200

    Update Greeting.js

--- source/src/components/Greeting.js
+++ source/src/components/Greeting.js
@@ -8,24 +8,24 @@ function Greeting() {
 
    useEffect(() => {
       function greetingText () {
-	      const date = new Date();
-		   const hour = date.getHours();
-		   if(hour<5) {
-		      setGreetingState('Good night!')
-		   } if (hour>=5 && hour<11) {
-	         setGreetingState('Good morning!')
-		   } if (hour>=11 && hour<17) {
-		      setGreetingState('Have a nice day!')
-		   } if (hour>=17 && hour<=23) {
-		      setGreetingState('Good evening!')
-		   } 
-		};
+	 const date = new Date();
+	 const hour = date.getHours();
+	 if(hour<5) {
+            setGreetingState('Good night!')
+	 } if (hour>=5 && hour<11) {
+	    setGreetingState('Good morning!')
+	 } if (hour>=11 && hour<17) {
+	    setGreetingState('Have a nice day!')
+	 } if (hour>=17 && hour<=23) {
+	    setGreetingState('Good evening!')
+	 } 
+      };
       greetingText();
    });
 
    return (
       <div>
-	      <div className='greeting'>{greetingState}</div>
+	 <div className='greeting'>{greetingState}</div>
       </div>
    )
 };
